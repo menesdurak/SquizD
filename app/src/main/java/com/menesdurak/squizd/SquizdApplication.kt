3cccb1b82d28942a49c342a5a096acbeb8a3282c
menesdurak <m.enes.durak@gmail.com>
2023-05-26 19:34:27 +0300

    Created application

---
+++ app/src/main/java/com/menesdurak/squizd/SquizdApplication.kt
@@ -0,0 +1,7 @@
+package com.menesdurak.squizd
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SquizdApplication : Application()
---
+++ app/src/main/java/com/menesdurak/squizd/presentation/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.menesdurak.squizd.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.menesdurak.squizd.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
