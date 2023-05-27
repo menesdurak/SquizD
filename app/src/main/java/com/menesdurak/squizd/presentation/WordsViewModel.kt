8e1f8d8caa546d8034e3e70cb8f2836810fa8212
menesdurak <m.enes.durak@gmail.com>
2023-05-27 18:39:57 +0300

    Created a view model for words

---
+++ app/src/main/java/com/menesdurak/squizd/presentation/WordsViewModel.kt
@@ -0,0 +1,36 @@
+package com.menesdurak.squizd.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.menesdurak.squizd.common.Resource
+import com.menesdurak.squizd.data.local.entity.Word
+import com.menesdurak.squizd.domain.use_case.words.AddWordUseCase
+import com.menesdurak.squizd.domain.use_case.words.GetAllWordsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class WordsViewModel @Inject constructor(
+    private val getAllWordsUseCase: GetAllWordsUseCase,
+    private val addWordUseCase: AddWordUseCase
+) : ViewModel() {
+
+    private val _wordsList = MutableLiveData<Resource<List<Word>>>(Resource.Loading)
+    val wordsList: LiveData<Resource<List<Word>>> = _wordsList
+
+    fun getAllWords() {
+        viewModelScope.launch {
+            _wordsList.value = Resource.Loading
+            _wordsList.value = getAllWordsUseCase()!!
+        }
+    }
+
+    fun addWord(word: Word) {
+        viewModelScope.launch {
+            addWordUseCase(word)
+        }
+    }
+}
