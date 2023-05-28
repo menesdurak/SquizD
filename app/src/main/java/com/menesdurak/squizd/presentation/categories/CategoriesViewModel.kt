d94cd7a06cdd714b3ee38fe65aae2778cf4fb110
menesdurak <m.enes.durak@gmail.com>
2023-05-28 09:06:08 +0300

    Created categories fragment and view model

---
+++ app/src/main/java/com/menesdurak/squizd/presentation/categories/CategoriesViewModel.kt
@@ -0,0 +1,36 @@
+package com.menesdurak.squizd.presentation.categories
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.menesdurak.squizd.common.Resource
+import com.menesdurak.squizd.data.local.entity.Category
+import com.menesdurak.squizd.domain.use_case.categories.AddCategoryUseCase
+import com.menesdurak.squizd.domain.use_case.categories.GetAllCategoriesUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class CategoriesViewModel @Inject constructor(
+    private val getAllCategoriesUseCase: GetAllCategoriesUseCase,
+    private val addCategoryUseCase: AddCategoryUseCase
+) : ViewModel() {
+
+    private val _categoriesList = MutableLiveData<Resource<List<Category>>>(Resource.Loading)
+    val categoriesList: LiveData<Resource<List<Category>>> = _categoriesList
+
+    fun getAllCategories() {
+        viewModelScope.launch {
+            _categoriesList.value = Resource.Loading
+            _categoriesList.value = getAllCategoriesUseCase()!!
+        }
+    }
+
+    fun addCategory(category: Category) {
+        viewModelScope.launch {
+            addCategoryUseCase(category)
+        }
+    }
+}
