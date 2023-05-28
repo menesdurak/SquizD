dc3d7e5ff4ac028f33f76b25b5eaebcc273de817
menesdurak <m.enes.durak@gmail.com>
2023-05-28 19:58:19 +0300

    Created get all words from category use case

---
+++ app/src/main/java/com/menesdurak/squizd/domain/use_case/words/GetAllWordsFromCategoryUseCase.kt
@@ -0,0 +1,20 @@
+package com.menesdurak.squizd.domain.use_case.words
+
+import com.menesdurak.squizd.common.Resource
+import com.menesdurak.squizd.data.local.entity.Word
+import com.menesdurak.squizd.domain.repository.LocalRepository
+import java.io.IOException
+import javax.inject.Inject
+
+class GetAllWordsFromCategoryUseCase @Inject constructor(
+    private val localRepository: LocalRepository
+) {
+
+    suspend operator fun invoke(categoryId: Int): Resource<List<Word>> {
+        return try {
+            Resource.Success(localRepository.getAllWordsFromCategory(categoryId))
+        } catch (e: IOException) {
+            Resource.Error(e)
+        }
+    }
+}
